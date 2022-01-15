d44c37b9a32177f2b2dd924697ebeeb7ad05a73c
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-01-15 10:37:33 +1000

    Added function to get most recent HH % value for given hospital.

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,7 @@
-// this is logging all the measure categories into the console - we will use these categories to generate the URLs for our API queries
-// we can perform similar requests to retrive the names/codes/locations of all hospitals ('reporting units')
-let measureCategoriesURL =
-  "https://myhospitalsapi.aihw.gov.au/api/v1/measure-categories";
-
-let getMeasureCategories = function (URL) {
-  fetch(URL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.result);
-    });
-};
-
-getMeasureCategories(measureCategoriesURL);
-
-// this is an example of what the data actually looks like when we retrieve it
-// main parameters: measure category = MYH-HH ('Hand hygiene %'), reporting unit code = H0235 ('Colac Area Health')
-// other (required) parameters: skip = 0 (how many entries to skip), top = 100 (how many entries to return)
-
 let reportingUnitsURL =
   "https://myhospitalsapi.aihw.gov.au/api/v1/reporting-units";
 
-let getReportingUnits = function () {
+let getHospitalsArray = function () {
   fetch(reportingUnitsURL)
     .then(function (response) {
       return response.json();
@@ -36,4 +15,17 @@ let getReportingUnits = function () {
     });
 };
 
-getReportingUnits();
+let getLatestHHPercentage = function (arr) {
+    console.log(arr.slice(-1)[0].value)
+  }
+
+let getHandWashingData = function (hospitalCode) {
+  let URL = `https://myhospitalsapi.aihw.gov.au/api/v1/flat-data-extract/MYH-HH?skip=0&top=100&measure_code=MYH0019&reporting_unit_code=${hospitalCode}`;
+  fetch(URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      getLatestHHPercentage(data.result.data);
+    });
+};
