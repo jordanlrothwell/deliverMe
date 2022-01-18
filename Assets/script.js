ba9afba892ad0d848dec4f2c546c0d5341d53053
Steven Phan <stevenphan@msn.com>
2022-01-19 00:37:00 +1100

    Made basic website functionality but still logging to console

--- Assets/script.js
+++ Assets/script.js
@@ -1,104 +1,19 @@
-<<<<<<< HEAD
-=======
-var userAddress = document.getElementById("input");
+var searchbar = document.querySelector("input");
+searchbar.focus();
 
-//event listener on user input (address autopopulated)
-// userAddress.addEventListener("submit", function (event) {
-//   event.preventDefault();
+var dropdown_items = document.querySelectorAll(".dropdown-item");
+dropdown_items[0].addEventListener("click", search);
 
-// fetch user addr
-
-// TODO: I dont have the user coord func, hardcoding the lon lat for now
-
-let lat = 53.32055555555556;
-let lon = -1.7297222222222221;
-
-getHospitalCoord();
-
-getClosestHospitals(lat, lon)
-    .then(function (hospitals) {
-      console.log(hospitals);
-    let closestHospitals = hospitals;
-      
-});
-
-
-
-function getClosestHospitals(lat, lon) {
-
-    return getHospitalCoord().then(function (hospitals) {
-        // loop thru each hospital, apply distance function
-
-        const hospitalWithDistance = hospitals.map(function (hospital) {
-            hospital.distance = distance(lat, hospital.latitude, lon, hospital.longitude);
-            return hospital;
-        });
-
-        hospitalWithDistance.sort(function (a, b) {
-            return b.distance - a.distance; // sort in asc order
-        });
-
-        return hospitalWithDistance.slice(0, 3);
-    })
-
+function search(){
+  const user_input = searchbar.value;
+  searchbar.value = "searching the universe...";
+  console.log(user_input);
+  searchbar.focus();
+  search_location(user_input);
 }
 
 
-
-function getHospitalCoord() {
-
-    const allHospitalsCoordinateData = [];
-
-    return fetch("https://myhospitalsapi.aihw.gov.au/api/v1/reporting-units?reporting_unit_type_code=H")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-
-            let hospitalsOnly = data.result;
-
-            for (i = 0; i < hospitalsOnly.length; i++) {
-                let hospitalCoordinateData = {};
-                hospitalCoordinateData.latitude = (hospitalsOnly[i].latitude)
-                hospitalCoordinateData.longitude = (hospitalsOnly[i].longitude)
-                hospitalCoordinateData.hospital_code = (hospitalsOnly[i].reporting_unit_code)
-                allHospitalsCoordinateData.push(hospitalCoordinateData);
-            }
-            return allHospitalsCoordinateData;
-        });
-
-}
-
-function distance(lat1, lat2, lon1, lon2) {
-
-    // The math module contains a function
-    // named toRadians which converts from
-    // degrees to radians.
-    lon1 = lon1 * Math.PI / 180;
-    lon2 = lon2 * Math.PI / 180;
-    lat1 = lat1 * Math.PI / 180;
-    lat2 = lat2 * Math.PI / 180;
-
-    // Haversine formula
-    let dlon = lon2 - lon1;
-    let dlat = lat2 - lat1;
-    let a = Math.pow(Math.sin(dlat / 2), 2)
-        + Math.cos(lat1) * Math.cos(lat2)
-        * Math.pow(Math.sin(dlon / 2), 2);
-
-    let c = 2 * Math.asin(Math.sqrt(a));
-
-    // Radius of earth in kilometers. Use 3956
-    // for miles
-    let r = 6371;
-
-    // calculate the result
-    return (c * r);
-}
-
->>>>>>> main
-
-let getData = function (hospitalCode) {
+function getData(hospitalCode, hospitalName) {
   let URL = "https://myhospitalsapi.aihw.gov.au/api/v1/reporting-units/" + hospitalCode + "/data-items";
   fetch(URL)
     .then(function (response) {
@@ -125,55 +40,67 @@ let getData = function (hospitalCode) {
       let IVFUnit = specialisedServicesData.filter((obj) => {
         return obj.reported_measure_code === "MYH-RM0778";
       });
-      
+
 
 
       if (handHygieneData && handHygieneData != 0) {
-        var returnHygieneData = handHygieneData[handHygieneData.length-1].value;
+        var returnHygieneData = handHygieneData[handHygieneData.length - 1].value;
       } else {
         var returnHygieneData = "NA";
       }
+      if (returnHygieneData == null) {
+        returnHygieneData = "NA";
+      }
 
-      if (staphInfectionData && staphInfectionData !=0) {
-        var returnStaphInfectionData = staphInfectionData[staphInfectionData.length-1].value;
+      if (staphInfectionData && staphInfectionData != 0) {
+        var returnStaphInfectionData = staphInfectionData[staphInfectionData.length - 1].value;
       } else {
         var returnStaphInfectionData = "NA";
       }
+      if (returnStaphInfectionData == null) {
+        returnStaphInfectionData = "NA";
+      }
 
       if (specialisedServicesData && specialisedServicesData != 0) {
-        var returnSpecialisedServicesData = specialisedServicesData[specialisedServicesData.length-1].value;
+        var returnSpecialisedServicesData = specialisedServicesData[specialisedServicesData.length - 1].value;
       } else {
         var returnSpecialisedServicesData = "NA";
       }
-      
+      if (returnSpecialisedServicesData == null) {
+        returnSpecialisedServicesData = "NA";
+      }
+
       if (obstetricsUnit && obstetricsUnit != 0) {
-        var returnObstetricsUnit = obstetricsUnit[obstetricsUnit.length-1].value;
+        var returnObstetricsUnit = obstetricsUnit[obstetricsUnit.length - 1].value;
       } else {
         var returnObstetricsUnit = "NA";
       }
+      if (returnObstetricsUnit == null) {
+        returnObstetricsUnit = "NA";
+      }
 
       if (neoNatalUnit && neoNatalUnit != 0) {
-        var returnNeoNatalUnit = neoNatalUnit[neoNatalUnit.length-1].value;
+        var returnNeoNatalUnit = neoNatalUnit[neoNatalUnit.length - 1].value;
       } else {
-        var returnNeoNatalUnit = "NA";        
+        var returnNeoNatalUnit = "NA";
       }
-
+      if (returnNeoNatalUnit == null) {
+        returnNeoNatalUnit = "NA";
+      }
 
       if (IVFUnit && IVFUnit != 0) {
-        var returnIVFUnit = IVFUnit[IVFUnit.length-1].value;
+        var returnIVFUnit = IVFUnit[IVFUnit.length - 1].value;
       } else {
-          var returnIVFUnit = "NA";
+        var returnIVFUnit = "NA";
+      }
+      if (returnIVFUnit == null) {
+        returnIVFUnit = "NA";
       }
 
-
-
-      console.log([resultsArray[0].reporting_unit_summary.reporting_unit_name, returnHygieneData, returnStaphInfectionData, returnSpecialisedServicesData, returnObstetricsUnit, returnNeoNatalUnit, returnIVFUnit]);
+      console.log([hospitalName, returnHygieneData, returnStaphInfectionData, returnSpecialisedServicesData, returnObstetricsUnit, returnNeoNatalUnit, returnIVFUnit]);
     });
-};
+}
 
-
-
-<<<<<<< HEAD
 function distance(lat1,
   lat2, lon1, lon2)
 {
@@ -213,60 +140,68 @@ let measureCategoriesURL =
   var sortedDistances_x;
 
 
-var test_variable;
-var temp;
-var test_location = "32 Newenden Street Maddington";
-var search_location = function(query) {
-  let URL = "https://api.tomtom.com/search/2/geocode/" + query + ".json?storeResult=false&countrySet=AU&view=Unified&key=Nb6yeAUhcbqCYWfTg0Q980Ek8XViGDYz"
+function search_location(query) {
+  let URL = "https://api.tomtom.com/search/2/geocode/" + query + ".json?storeResult=false&countrySet=AU&view=Unified&key=Nb6yeAUhcbqCYWfTg0Q980Ek8XViGDYz";
   console.log(query);
   fetch(URL)
-  .then(response => response.json())
-  .then(data => {
-    test_variable = data;
-    console.log(data);
-    console.log("the closest address is " + data.results[0].address.streetName + " " + data.results[0].address.municipalitySubdivision + " " + data.results[0].id)
-    var lat = test_variable.results[0].position.lat;
-    var lon = test_variable.results[0].position.lon;
-    console.log("lat: " + lat + ", lon: " + lon);
-    return([lat,lon]);
-  })
-  .then(coords => {
-    var lat = coords[0];
-    var lon = coords[1];
-    fetch("https://myhospitalsapi.aihw.gov.au/api/v1/reporting-units")
     .then(response => response.json())
     .then(data => {
-      console.log(data);
       test_variable = data;
-      var distances = [];
-      for (let i = 0; i < data.result.length; i++) {
-        // console.log("distance to " + data.result[i].reporting_unit_name + " is " + distance(lat, data.result[i].latitude, lon, data.result[i].longitude).toFixed(2) + "km");
-        distances[i] = distance(lat, data.result[i].latitude, lon, data.result[i].longitude);
-      }
-      distances_x = distances;
-      var sortedDistances = distances.slice(0);
-      sortedDistances.sort(function(a, b) { return a - b; });
-      sortedDistances_x = sortedDistances;
-      closestHospitalCodes = [];
-      closestHospitalNames = [];
-      for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < data.result.length; j++){
-          if (sortedDistances[i] == distances[j]) {
-            closestHospitalCodes.push(data.result[j].reporting_unit_code);
-            closestHospitalNames.push(data.result[j].reporting_unit_name);
+      console.log(data);
+      console.log("the closest address is " + data.results[0].address.streetName + " " + data.results[0].address.municipalitySubdivision + " " + data.results[0].id);
+      var lat = test_variable.results[0].position.lat;
+      var lon = test_variable.results[0].position.lon;
+      console.log("lat: " + lat + ", lon: " + lon);
+      return ([lat, lon]);
+    })
+    .then(coords => {
+      var lat = coords[0];
+      var lon = coords[1];
+      fetch("https://myhospitalsapi.aihw.gov.au/api/v1/reporting-units")
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          test_variable = data;
+          var distances = [];
+          for (let i = 0; i < data.result.length; i++) {
+            // console.log("distance to " + data.result[i].reporting_unit_name + " is " + distance(lat, data.result[i].latitude, lon, data.result[i].longitude).toFixed(2) + "km");
+            distances[i] = distance(lat, data.result[i].latitude, lon, data.result[i].longitude);
           }
-        }
-      }
-      uniqueHospitalCodes = [...new Set(closestHospitalCodes)];
-      console.log(uniqueHospitalCodes);
-      return(uniqueHospitalCodes)
-    })
-    .then(codes => {
-
-    })
-  })
+          distances_x = distances;
+          var sortedDistances = distances.slice(0);
+          sortedDistances.sort(function (a, b) { return a - b; });
+          sortedDistances_x = sortedDistances;
+          closestHospitalCodes = [];
+          closestHospitalNames = [];
+          for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < data.result.length; j++) {
+              if (sortedDistances[i] == distances[j]) {
+                closestHospitalCodes.push(data.result[j].reporting_unit_code);
+                closestHospitalNames.push(data.result[j].reporting_unit_name);
+              }
+            }
+          }
+          return ([closestHospitalCodes, closestHospitalNames]);
+        })
+        .then(codes => {
+          for (let i = 0; i < codes[0].length; i++) {
+            getData(codes[0][i], codes[1][i]);
+          }
+        });
+    });
 }
 
-search_location(test_location);
-=======
->>>>>>> main
+
+
+
+// cardHTML[i] =   '<div class="card col-12 col-sm-6 col-md-4 col-lg-4 col-xl-2 text-white bg-primary">' +
+//                                         '<div class="card-body">' +
+//                                             '<h5 class="card-title">' + returnArray[i][8] + '</h5>' +
+//                                             '<img src="' + returnArray[i][3] + '">' +
+//                                             '<p class="card-text"><strong>' + returnArray[i][1] + ':</strong> ' + returnArray[i][2] + '</p>' +
+//                                             '<p class="card-text"><strong>Temperature:</strong> ' + returnArray[i][4] + '&degC</p>' +
+//                                             '<p class="card-text"><strong>Humidity:</strong> ' + returnArray[i][5] + '%</p>' +
+//                                             '<p class="card-text"><strong>Wind speed:</strong> ' + returnArray[i][6] + 'm/s</p>' +
+//                                             '<p class="card-text"><strong>UV index:</strong> ' + rate_UV(returnArray[i][7]) + '</p>' +
+//                                         '</div>' + 
+//                                     '</div>';
